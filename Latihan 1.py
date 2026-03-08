38162f1ac2ffdc34514285a36be5b1df54fc65df
Ikmwfy <01dgu24f1054@student.puo.edu.my>
2026-03-09 00:51:00 +0800

    Update Latihan 1.py

--- Latihan 1.py
+++ Latihan 1.py
@@ -88,7 +88,7 @@ if check_password():
         <style>
         .sidebar-profile-container {{
             position: relative;
-            width: 120%;
+            width: 100%;
             height: 200px;
             border-radius: 15px;
             overflow: hidden;
@@ -335,6 +335,7 @@ if check_password():
             else: st.error("❌ Kolum STN, E, N tak jumpa dalam CSV!")
 
         except Exception as e: st.error(f"❌ Ada ralat: {e}")
+
 
 
 
