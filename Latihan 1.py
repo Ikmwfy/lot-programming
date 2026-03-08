f2ab0bfba7b1af4f99ed3b516f1340d31872cbb4
Ikmwfy <01dgu24f1054@student.puo.edu.my>
2026-03-08 18:16:36 +0800

    Update Latihan 1.py

--- Latihan 1.py
+++ Latihan 1.py
@@ -92,9 +92,9 @@ if check_password():
                 align-items: center; background-color: #000;
             }}
             .video-bg {{
-                position: absolute; top: 50%; left: 0%; min-width: 100%;
+                position: absolute; top: 50%; left: -50%; min-width: 100%;
                 min-height: 100%; width: auto; height: auto; z-index: 0;
-                transform: translate(-50%); opacity: 0.6;
+                transform: translate(-50%,-50%); opacity: 0.6;
             }}
             .header-content {{
                 position: relative; z-index: 1; display: flex; align-items: center;
@@ -274,6 +274,7 @@ if check_password():
             else: st.error("❌ Kolum STN, E, N tak jumpa dalam CSV!")
 
         except Exception as e: st.error(f"❌ Ada ralat: {e}")
+
 
 
 
