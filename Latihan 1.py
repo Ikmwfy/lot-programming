0ae8b6c2a9f4bb714722b34d3af99a52b8b6d260
Ikmwfy <01dgu24f1054@student.puo.edu.my>
2026-03-09 02:01:47 +0800

    Update Latihan 1.py

--- Latihan 1.py
+++ Latihan 1.py
@@ -8,8 +8,8 @@ import numpy as np
 from shapely.geometry import Polygon, Point, LineString, mapping
 import json
 import os
-import folium 
-from streamlit_folium import folium_static 
+import folium
+from streamlit_folium import st_folium
 from pyproj import Transformer
 import base64
 
@@ -337,7 +337,7 @@ if check_password():
 
                     if show_luas_label:
                         folium.Marker([df['lat'].mean(), df['lon'].mean()], icon=folium.DivIcon(html=f'<div style="font-size: {label_size_luas}pt; color: #00FF00; text-shadow: 3px 3px 5px black; font-weight: 900; width: 250px; text-align: center; margin-left: -125px;">{area:.2f} m²</div>')).add_to(m)
-                    folium_static(m, width=1400, height=600)
+                    st_folium(m, width=1400, height=600)
 
                 else:
                     # --- MOD MATPLOTLIB ---
@@ -374,3 +374,4 @@ if check_password():
             else: st.error("❌ Kolum STN, E, N tak jumpa dalam CSV!")
 
         except Exception as e: st.error(f"❌ Ada ralat: {e}")
+
