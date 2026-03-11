e537379854402e8ba3fc45d844257a34ce6341ef
Ikmwfy <01dgu24f1054@student.puo.edu.my>
2026-03-12 02:53:57 +0800

    Update Latihan 1.py

--- Latihan 1.py
+++ Latihan 1.py
@@ -368,7 +368,13 @@ if check_password():
 
                     if show_luas_label:
                         folium.Marker([df['lat'].mean(), df['lon'].mean()], icon=folium.DivIcon(html=f'<div style="font-size: {label_size_luas}pt; color: #00FF00; text-shadow: 3px 3px 5px black; font-weight: 900; width: 250px; text-align: center; margin-left: -125px;">{area:.2f} m²</div>')).add_to(m)
-                    st_folium(m, width=1400, height=600)
+                    st_folium(
+                        m, 
+                        width=1400, 
+                        height=600, 
+                        returned_objects=[], 
+                        key="peta_survey"
+                    )
 
                 else:
                     # --- MOD MATPLOTLIB ---
@@ -405,6 +411,7 @@ if check_password():
             else: st.error("❌ Kolum STN, E, N tak jumpa dalam CSV!")
 
         except Exception as e: st.error(f"❌ Ada ralat: {e}")
+
 
 
 
