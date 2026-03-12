8b6298ee621c62a9017a7e08cb7c7cf3ff5d17bc
Ikmwfy <01dgu24f1054@student.puo.edu.my>
2026-03-12 10:20:58 +0800

    Update Latihan 1.py

--- Latihan 1.py
+++ Latihan 1.py
@@ -304,10 +304,6 @@ if check_password():
 
     # ================== BACA DATA ==================
     # ================== BACA DATA ==================
-    # Letak di bahagian atas skrip atau sebelum pembacaan fail
-    if uploaded_file is not None and "first_load" not in st.session_state:
-        st.session_state.show_map = True
-        st.session_state.first_load = True
         
         try:
             df = pd.read_csv(uploaded_file)
@@ -483,6 +479,7 @@ if check_password():
             else: st.error("❌ Kolum STN, E, N tak jumpa dalam CSV!")
 
         except Exception as e: st.error(f"❌ Ada ralat: {e}")
+
 
 
 
